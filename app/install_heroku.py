92c9da291caec293a372c2cf2a3e8a900cb08f03
ksks <nose2002kr@gmail.com>
2024-06-21 18:17:34 +0900

    remove install_heroku script because buildpack:add was found.

--- app/install_heroku.py
+++
@@ -1,6 +0,0 @@
-import os
-from loguru import logger
-
-logger.debug('heroku cli install start')
-os.system('curl https://cli-assets.heroku.com/install.sh | sh')
-logger.debug('heroku cli installed')
